edf4021306256e176f25704f743f28cdaa740f65
hady_amrr <hadyamr39@gmail.com>
2025-05-08 09:24:59 +0300

    Testing

---
+++ test copy.cpp
@@ -0,0 +1,10 @@
+#include<iostream>
+using namespace std;
+int main(){
+    cout<<"Hello world "<<endl;
+    cout<<"test2"<<endl;
+    cout<<"test3"<<endl;
+    cout<<"test3"<<endl;
+    cout<<"test3"<<endl;
+    cout<<"test3"<<endl;
+    }
